46c1f493bff0a424c2a1bc8522c0de5c946623cf
Tiago <tiago@pop-os.localdomain>
2021-01-26 17:43:59 -0300

    Program start

---
+++ copy-oci-bucket.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# copy-ocy-bucket.sh - Copy files to a OCI Bucket
+#
+# Site:       https://github.com/tiagorosa80/copy-oci-bucket
+# Autor:      Tiago da Rosa
+#
+# ------------------------------------------------------------------------ #
+#  This program is used to copy files to a Bucket on the Oracle Cloud.
+#  It is an alternative to the oci cli.
+#  My intention is to make copying multiple files easy.
+#
+#  Example:
+#      $ ./copy-ocy-bucket.sh -c conf.cf
+#      In this example the script will copy the files to Bucket that are
+#      informed in the configuration file.
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 26/01/2020, Tiago da Rosa:
+#       - Program Start
+#
+#
+# ------------------------------------------------------------------------ #
+# Tested on:
+#   bash 5.0.17(1)-release
+#   oci cli 2.19.0
+#       - https://docs.oracle.com/en-us/iaas/tools/oci-cli/2.19.0/oci_cli_docs/index.html
+#       - https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/cliinstall.htm
+# ------------------------------------------------------------------------ #
+# Thanks to:
+#
+#
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- TESTES ----------------------------------------- #
+
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- FUNÇÕES ----------------------------------------- #
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+
+# ------------------------------------------------------------------------ #
